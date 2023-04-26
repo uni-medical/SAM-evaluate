88fc76a83f41259f858b45d4bd3a246b358a5b49
Junlong <92080553+Cjl-MedSeg@users.noreply.github.com>
2023-04-26 20:24:12 +0800

    Update data_load.py

--- data_load.py
+++ data_load.py
@@ -9,6 +9,7 @@ import numpy as np
 from skimage.measure import label, regionprops
 from matplotlib import pyplot as plt
 import random
+import json
 
 def show_mask(mask, ax, random_color=False):
     if random_color:
@@ -90,60 +91,64 @@ def form_mask_get_box(mask, std = 0.1, max_pixel = 20):
     y1 = y1 + noise_y
     return torch.as_tensor(np.array([(x0, y0, x1, y1)]), dtype=torch.float)
 
-def get_box(mask, num_classes=4, std = 0.1, max_pixel = 20):
+def get_box(input_mask, num_classes=4, std = 0.1, max_pixel = 20):
     # 标记图像并获取区域属性
-    mask = np.array(mask)
-    label_img = label(mask)
-    regions = regionprops(label_img)
-    # 迭代所有区域并获取边界框坐标
-    boxes = []
-    for region in regions:
-        minr, minc, maxr, maxc = region.bbox
-        boxes.append((minc, minr, maxc, maxr))  # 坐标存储为 (x_min, y_min, x_max, y_max) 的形式
-    # 如果生成的边界框数量大于类别数，则按区域面积排序并选择前n个区域
-    if len(boxes) > num_classes:
-        sorted_regions = sorted(regions, key=lambda x: x.area, reverse=True)[:num_classes]
+    masks = np.array(input_mask)
+    total_boxes = []
+    for i in range(masks.shape[0]):
+        mask = masks[i]
+        label_img = label(mask)
+        regions = regionprops(label_img)
+        # 迭代所有区域并获取边界框坐标
         boxes = []
-        for region in sorted_regions:
+        for region in regions:
             minr, minc, maxr, maxc = region.bbox
-            boxes.append((minc, minr, maxc, maxr))
-    elif len(boxes) < num_classes:
-        if len(boxes) == 0:
-            boxes.append([256, 256, 512, 512])
-        num_duplicates = num_classes - len(boxes)
-        for i in range(num_duplicates):
-            boxes.append(random.choice(boxes))
+            boxes.append((minc, minr, maxc, maxr))  # 坐标存储为 (x_min, y_min, x_max, y_max) 的形式
+        # 如果生成的边界框数量大于类别数，则按区域面积排序并选择前n个区域
+        if len(boxes) > num_classes:
+            sorted_regions = sorted(regions, key=lambda x: x.area, reverse=True)[:num_classes]
+            boxes = []
+            for region in sorted_regions:
+                minr, minc, maxr, maxc = region.bbox
+                boxes.append((minc, minr, maxc, maxr))
+        elif len(boxes) < num_classes:
+            if len(boxes) == 0:
+                boxes.append([256, 256, 512, 512])
+            num_duplicates = num_classes - len(boxes)
+            for i in range(num_duplicates):
+                boxes.append(random.choice(boxes))
 
-    noise_boxes = []
-    for box in boxes:
-        x0, y0 = box[0], box[1]
-        x1, y1 = box[2], box[3]   #start_point, end_point = (x0, y0), (x1, y1)
-        width = abs(x1 - x0)
-        height = abs(y1 - y0)
-        # 计算标准差和最大噪声值
-        noise_std = min(width, height) * std
-        max_noise = min(max_pixel, int(noise_std * 5))
-        # 在每个坐标上添加随机噪音,应用噪声到左上角坐标
-        if max_noise == 0:
-            noise_x = 0
-            noise_y = 0
-        else:
-            noise_x = np.random.randint(-max_noise, max_noise)
-            noise_y = np.random.randint(-max_noise, max_noise)
-        x0 = x0 + noise_x
-        y0 = y0 + noise_y
-        # 应用噪声到右下角坐标
-        if max_noise == 0:
-            noise_x = 0
-            noise_y = 0
-        else:
-            noise_x = np.random.randint(-max_noise, max_noise)
-            noise_y = np.random.randint(-max_noise, max_noise)
-        x1 = x1 + noise_x
-        y1 = y1 + noise_y
-        noise_boxes.append((x0, y0, x1, y1))
+        noise_boxes = []
+        for box in boxes:
+            x0, y0 = box[0], box[1]
+            x1, y1 = box[2], box[3]   #start_point, end_point = (x0, y0), (x1, y1)
+            width = abs(x1 - x0)
+            height = abs(y1 - y0)
+            # 计算标准差和最大噪声值
+            noise_std = min(width, height) * std
+            max_noise = min(max_pixel, int(noise_std * 5))
+            # 在每个坐标上添加随机噪音,应用噪声到左上角坐标
+            if max_noise == 0:
+                noise_x = 0
+                noise_y = 0
+            else:
+                noise_x = np.random.randint(-max_noise, max_noise)
+                noise_y = np.random.randint(-max_noise, max_noise)
+            x0 = x0 + noise_x
+            y0 = y0 + noise_y
+            # 应用噪声到右下角坐标
+            if max_noise == 0:
+                noise_x = 0
+                noise_y = 0
+            else:
+                noise_x = np.random.randint(-max_noise, max_noise)
+                noise_y = np.random.randint(-max_noise, max_noise)
+            x1 = x1 + noise_x
+            y1 = y1 + noise_y
+            noise_boxes.append((x0, y0, x1, y1))
 
-    return torch.as_tensor(noise_boxes, dtype=torch.float)
+        total_boxes.append(noise_boxes)
+    return torch.as_tensor(np.stack(total_boxes, axis=0), dtype=torch.float)
 
 
 def select_random_points(pred, gt, point_num = 9):
@@ -186,34 +191,36 @@ def select_random_points(pred, gt, point_num = 9):
 
 
 def random_point_sampling(img, point_num = 2):
-    img = np.array(img)
-
-    # 获取黑/白色像素的坐标
-    white_pixels = np.argwhere(img == 1)
-    # 从中随机选择num_points个点
-    if len(white_pixels) > 12:
-        coords = white_pixels[np.random.choice(white_pixels.shape[0], point_num, replace=False)]
-        coords[:, [0, 1]] = coords[:, [1, 0]]
-        labels = np.ones(shape=(point_num))
-
-    else:
-        #随机选择点
-        coords = []
-        for i in range(point_num):
-            x, y = np.random.randint(0, img.shape[0]), np.random.randint(0, img.shape[1])
-            coords.append((x, y))
-        labels = []
-        for coord in coords:
-            if img[coord[0], coord[1]] > 0:
-                labels.append(1)
-            else:
-                labels.append(0)
-        # print(np.array(coords).shape)
-        # print(np.array(labels).shape)
-
-    labels = torch.as_tensor(labels, dtype=torch.int)
-    coords = torch.as_tensor(coords, dtype=torch.float)
-    return coords, labels
+    images = np.array(img)
+    total_coords, total_labels = [],[]
+    for i in range(images.shape[0]):
+        img = images[i]
+        # 获取黑/白色像素的坐标
+        white_pixels = np.argwhere(img == 1)
+        # 从中随机选择num_points个点
+        if len(white_pixels) > 12:
+            coords = white_pixels[np.random.choice(white_pixels.shape[0], point_num, replace=False)]
+            coords[:, [0, 1]] = coords[:, [1, 0]]
+            labels = np.ones(shape=(point_num))
+   
+        else:
+            #随机选择点
+            coords = []
+            for i in range(point_num):
+                x, y = np.random.randint(0, img.shape[0]), np.random.randint(0, img.shape[1])
+                coords.append((x, y))
+            labels = []
+            for coord in coords:
+                if img[coord[0], coord[1]] > 0:
+                    labels.append(1)
+                else:
+                    labels.append(0)
+        total_coords.append(coords)
+        total_labels.append(labels)
+  
+    coords_ = torch.as_tensor(np.stack(total_coords, axis=0), dtype=torch.float)
+    labels_ = torch.as_tensor(np.stack(total_labels, axis=0), dtype=torch.int)
+    return coords_, labels_
 
 
 def transforms(img_size):
@@ -223,8 +230,34 @@ def transforms(img_size):
         ], p=1.)
 
 
+def mask_to_one_hot(mask, num_classes):
+    """
+    将二维掩码 (width, height) 转化为one-hot编码 (num_classes, width, height)
+
+    Args:
+    mask: 二维, shape 为 [width, height] 的 numpy 数组。
+    num_classes (int): 标签的数量。
+
+    Returns:
+    numpy.ndarray: 形状为 [num_classes, width, height] 的 one-hot 编码。
+    """
+    one_hot = np.eye(num_classes)[mask]
+    # transpose the array to have the shape [num_classes, width, height]
+    one_hot = one_hot.transpose(2, 0, 1)
+    return one_hot
+
+
 class Data_Loader(Dataset):
-    def __init__(self, data_path, image_size = None, requires_name = True, mode = 'train', prompt_point=True, prompt_box=True, num_boxes = 1, num_point = 1):
+    def __init__(self, 
+    data_path, 
+    image_size = None, 
+    requires_name = True, 
+    mode = 'train', 
+    prompt_point=True, 
+    prompt_box=True, 
+    num_boxes = 3, 
+    num_point = 3,
+    ):
         # 初始化函数，读取所有data_path下的图片
         self.image_size = image_size
         self.data_path = data_path
@@ -232,8 +265,11 @@ class Data_Loader(Dataset):
         self.img_list = [line.strip() +".png" for line in open(os.path.join(data_path, mode +".txt"), "r").readlines()]
         self.imgs_path = [os.path.join(data_path, "images", img) for img in self.img_list]
         self.label_path = [os.path.join(data_path, "masks", img) for img in self.img_list]
-        #self.imgs_path = [f for f in glob.glob(os.path.join(data_path, 'images', mode, '*.*')) if f.endswith(('.jpg', '.png', '.jpeg', '.tif'))]
-        #self.label_path = [f for f in glob.glob(os.path.join(data_path, 'masks', mode, '*.*')) if f.endswith(('.jpg', '.png', '.jpeg', '.tif'))]
+
+        with open(os.path.join(data_path, 'dataset.json'), 'r') as f:
+            data = json.load(f)
+        key = data_path.split('/')[-1]
+        self.num_class = data[key]
 
         self.imgs_path.sort()
         self.label_path.sort()
@@ -250,26 +286,22 @@ class Data_Loader(Dataset):
         image = cv2.imread(self.imgs_path[index])
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         mask = cv2.imread(self.label_path[index], 0)
-        if mask.max() == 255:
-            print("Mask is not a binary map")
-            mask = mask // 255.
-
+  
         # 读取训练图片和标签图片
         augments = self.transforms(image=image, mask=mask)
-        # image, mask = augments['image'].transpose(2, 0, 1), augments['mask'].astype(np.int64)
         image, mask = augments['image'], augments['mask'].astype(np.int64)
 
-        image_input["image"] = image
-        image_input["label"] = np.expand_dims(mask, axis=0)
 
+        image = np.repeat(np.expand_dims(image, axis=0), repeats=self.num_class-1, axis=0)
+        mask = mask_to_one_hot(mask, self.num_class)[1:]
+     
+        image_input["image"] = image
+        image_input["label"] = mask
+ 
         if self.prompt_point:
             point, point_label = random_point_sampling(mask, point_num = self.point_num)
-            # draw_boxes(mask.cpu().numpy()*255, box.cpu().numpy())
             image_input["point_coords"] = point
             image_input["point_labels"] = point_label
-
-            # image_input["point_coords"] = point.unsqueeze(1)
-            # image_input["point_labels"] = point_label.unsqueeze(1)
 
         if self.prompt_box:
             box = get_box(mask, num_classes=self.boxes_num)
@@ -289,11 +321,14 @@ class Data_Loader(Dataset):
 
 
 if __name__ == "__main__":
-    train_dataset = Data_Loader("/home/chengjunlong//mount_preprocessed_sam/2d/semantic_seg/fundus_photography/drac2022_taska1/", image_size=1024, mode='train', prompt_point=True, prompt_box=False)
+    train_dataset = Data_Loader('mount_preprocessed_sam/2d/semantic_seg/microscopy/CellTracking2d/', image_size=1024, mode='val', prompt_point=True, prompt_box=True)
     print("数据个数：", len(train_dataset))
     train_batch_sampler = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=4, shuffle=False)
     for batched_image in (train_batch_sampler):
+        print('*'*10)
         print(batched_image['image'].shape)
         print(batched_image['label'].shape)
-        print(torch.unique(batched_image['label']))
+        print(batched_image['point_coords'].shape)
+        print(batched_image['point_labels'].shape)
+        # print(torch.unique(batched_image['label']))
         # print(batched_image.get('boxes', None))
